86434af5f5269dd052d8095f2e9d6496ba75a1dd
Aditya Samavedam <aaramans@gmail.com>
2017-01-12 16:33:38 +0530

    Added code for sign in

--- client/scripts/index.js
+++ client/scripts/index.js
@@ -5,14 +5,33 @@
 
     app.controller('NavController', ['$scope', function ($scope) {
         $scope.isSignedIn = false;
-        $scope.userName = "Aditya";
+        $scope.user = {};
 
         $scope.signIn = function () {
-            $scope.isSignedIn = true;
+            $scope.signInObj = {};
+            $('.login_box').modal('show');
         };
 
         $scope.signOut = function () {
             $scope.isSignedIn = false;
         };
     }]);
+
+    app.directive('loginModal', function(){
+        return {
+            restrict : 'E',
+            replace : true,
+            templateUrl : 'views/login.html',
+            controller : 'NavController',
+            link : function (scope, $elem, $attr) {
+                scope.signInObj = {};
+                scope.signInSubmit = function(isValid){
+                    if(isValid){
+                        console.log('submitting the form',scope.signInObj);
+                        //$scope.isSignedIn = true;
+                    }
+                };
+            }
+        };
+    })
 })();
