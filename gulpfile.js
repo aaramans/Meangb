5f3bc882f66e01c4142b06c57e2aa08fb6e157fa
Aditya Samavedam <aaramans@gmail.com>
2017-02-14 18:28:56 +0530

    changes in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     args = require('yargs').argv,
-    $g = require('gulp-load-plugins')({lazy : true}),
+    $g = require('gulp-load-plugins')({
+        lazy: true
+    }),
     browserSync = require('browser-sync'),
     del = require('del'),
     config = require('./gulp.config')(),
@@ -10,8 +12,8 @@ function log(msg) {
     $g.util.log(msg);
 }
 
-function startBrowserSync(){
-    if(args.nosync || browserSync.active){
+function startBrowserSync() {
+    if (args.nosync || browserSync.active) {
         return;
     }
 
@@ -20,22 +22,22 @@ function startBrowserSync(){
 
     log('Starting Browser Sync with options...');
     var bSOptions = {
-        proxy : "localhost:" + port,
-        port : 3000,
-        files : [
+        proxy: "localhost:" + port,
+        port: 3000,
+        files: [
             config.client + "assets/css/*.*",
             config.client + "assets/js/*.*",
             config.client + "index.html"
         ],
-        ghostMode : {
-            clicks : true,
-            location : false,
-            forms : true,
-            scroll : true
+        ghostMode: {
+            clicks: true,
+            location: false,
+            forms: true,
+            scroll: true
         },
-        injectChanges : true,
-        notify : true,
-        reloadDelay : 1000
+        injectChanges: true,
+        notify: true,
+        reloadDelay: 1000
     };
 
     browserSync(bSOptions);
@@ -43,12 +45,16 @@ function startBrowserSync(){
 
 gulp.task('styles', ['clear'], function () {
     log('Compiling Sass to css...');
-	return gulp.src(config.filePaths.allSass)
+    return gulp.src(config.filePaths.allSass)
         .pipe($g.plumber())
-		.pipe($g.sass({style: 'expanded'}))
-        .pipe($g.autoprefixer({browsers: ['last 2 version']}))
+        .pipe($g.sass({
+            style: 'expanded'
+        }))
+        .pipe($g.autoprefixer({
+            browsers: ['last 2 version']
+        }))
         .pipe($g.concatCss(config.cssConcatFname))
-		.pipe(gulp.dest(config.filePaths.cssDest));
+        .pipe(gulp.dest(config.filePaths.cssDest));
 });
 
 gulp.task('clear', function () {
@@ -60,32 +66,32 @@ gulp.task('clear', function () {
 
 gulp.task('scripts', function () {
     log('Compiling js...');
-	return gulp.src(config.filePaths.allJs)
-		.pipe($g.uglify())
-		.pipe($g.concat(config.jsConcatFname))
-		.pipe(gulp.dest(config.filePaths.jsDest));
+    return gulp.src(config.filePaths.allJs)
+        .pipe($g.uglify())
+        .pipe($g.concat(config.jsConcatFname))
+        .pipe(gulp.dest(config.filePaths.jsDest));
 });
 
 gulp.task('lint', function () {
     log('Linting js...');
     return gulp.src(config.filePaths.allJs)
-		.pipe($g.jscs())
+        .pipe($g.jscs())
         .pipe($g.jshint())
         //.pipe($g.jshint.reporter('jshint-stylish', {verbose: true}))
-		.pipe($g.jshint.reporter('default'));
+        .pipe($g.jshint.reporter('default'));
 });
 
 gulp.task('watch', function () {
     log('Found File changes. Triggering compilation...');
-	gulp.watch(config.filePaths.allJs, ['scripts']);
-	gulp.watch(config.filePaths.allSass, ['styles']);
+    gulp.watch(config.filePaths.allJs, ['scripts']);
+    gulp.watch(config.filePaths.allSass, ['styles']);
 });
 
 gulp.task('wiredep', function () {
     log('wire up bower css and js and appjs into html...');
     var options = config.getDefOptWiredep(),
         wiredep = require('wiredep').stream;
-    
+
     return gulp
         .src(config.filePaths.html)
         .pipe(wiredep(options))
@@ -104,13 +110,13 @@ gulp.task('inject', ['wiredep', 'styles'], function () {
 gulp.task('start-server', ['inject'], function () {
     var isDev = true,
         nodeMonOptions = {
-            script : config.nodeServer,
-            delay : 1,
-            env : {
-                'PORT' : port,
-                'NODE_ENV' : isDev ? 'dev' : 'build'
+            script: config.nodeServer,
+            delay: 1,
+            env: {
+                'PORT': port,
+                'NODE_ENV': isDev ? 'dev' : 'build'
             },
-            watch : [config.filePaths.server]
+            watch: [config.filePaths.server]
         };
     return $g.nodemon(nodeMonOptions)
         .on('start', function () {
@@ -119,10 +125,12 @@ gulp.task('start-server', ['inject'], function () {
         }).on('restart', function (ev) {
             log('** Nodemon restarted **');
             log('** Files changed:\n' + ev);
-            setTimeout(function(){
+            setTimeout(function () {
                 browserSync.notify('Relaoding the browser');
-                browserSync.reload({stream : false});
-            },config.browserReloadDelay);
+                browserSync.reload({
+                    stream: false
+                });
+            }, config.browserReloadDelay);
         }).on('crash', function () {
             log('** Nodemon crashed **');
         }).on('exit', function () {
@@ -130,5 +138,5 @@ gulp.task('start-server', ['inject'], function () {
         });
 });
 
-gulp.task('help',$g.taskListing);
+gulp.task('help', $g.taskListing);
 gulp.task('default', ['help']);
